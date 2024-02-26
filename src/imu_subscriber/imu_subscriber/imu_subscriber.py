e014d16b64fabb92eba157f9b6df793cc31f1ece
yunzhewong <yunzhe.wong@gmail.com>
2024-02-26 13:29:42 +1100

    simple real time plotting

--- src/imu_subscriber/imu_subscriber/imu_subscriber.py
+++ src/imu_subscriber/imu_subscriber/imu_subscriber.py
@@ -2,35 +2,53 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import Imu
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-class MinimalSubscriber(Node):
-
+class IMUSubscriber(Node):
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('imu_subscriber')
         self.subscription = self.create_subscription(
             Imu,
             '/imu_plugin/out',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
+        self.times = []
+        self.data = []
+        self.i = 0
 
     def listener_callback(self, msg: Imu):
         self.get_logger().info('I heard: "%s"' % msg.angular_velocity)
         self.get_logger().info('I heard: "%s"' % msg.linear_acceleration)
 
+        self.times.append(self.i)
+        self.data.append(msg.linear_acceleration.z)
+
+        if (len(self.times) > 50):
+            self.times.pop(0)
+            self.data.pop(0)
+          
+        plt.clf()
+
+        plt.plot(self.times, self.data, linewidth=2.0, color="blue")
+
+        self.i += 1
+        plt.pause(0.1)
+
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = MinimalSubscriber()
+    imu_subscriber = IMUSubscriber()
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(imu_subscriber)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    imu_subscriber.destroy_node()
     rclpy.shutdown()
 
 
