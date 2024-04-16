7608d9054c27fa887588404f32e2c1221012c9b8
Ivano-Asakiri <73212083+Ivano-Asakiri@users.noreply.github.com>
2024-04-16 15:28:17 +1000

    Add files via upload

---
+++ physical_motor/aiosv2_alter.py
@@ -0,0 +1,55 @@
+import socket
+import aios
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.settimeout(2)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+PORT_rt = 2333  # Real-time control data port, ie. speed, position, current and other real-time data
+PORT_srv = 2334  # Low priority service data port. ie, parameter setting and reading
+PORT_pt = 10000  # Passthrough port
+
+NETWORK = "10.10.10.255"
+
+
+class ConnectedAddresses:
+    def __init__(self, ips):
+        self.ips = ips
+
+    def enable(self):
+        for ip in self.ips:
+            aios.enable(ip, 1)
+
+    def disable(self):
+        for ip in self.ips:
+            aios.disable(ip, 1)
+
+    def get_motor_ips(self):
+        motor_ips = []
+        for ip in self.ips:
+            root = aios.getRoot(ip)
+            is_control_box = root.get("deviceType", None) == "ctrlbox"
+            if not is_control_box:
+                motor_ips.append(ip)
+
+        return motor_ips
+
+
+def get_addresses():
+    s.sendto("Is any AIOS server here?".encode("utf-8"), (NETWORK, PORT_srv))
+    all_ips = []
+    while True:
+        try:
+            _, address = s.recvfrom(1024)
+            all_ips.append(address[0])
+            found_server = True
+        except socket.timeout:  # fail after 1 second of no activity
+            if found_server:
+                return ConnectedAddresses(all_ips)
+            raise Exception("No Addresses Found")
+
+
+def separate_ips(ips):
+
+    print()
---
+++ physical_motor/test_alter.py
@@ -0,0 +1,47 @@
+import time
+import aios
+import aiosv2
+
+connected_addresses = aiosv2.get_addresses()
+
+
+if connected_addresses is not None:
+    print("INITIALIZATION")
+    connected_addresses.enable()
+    motor_ips = connected_addresses.get_motor_ips()
+
+    time.sleep(2)
+
+    print("MOVEMENT") 
+    if motor_ips is not None:
+        for motor_ip in motor_ips:
+            
+            p, v, c = aios.getCVP(motor_ip, 1)
+            print('motor IP: ', motor_ip)
+            print('Position', p)
+            print('Velocity', v)
+            print('Current', c)
+
+            aios.controlMode(aios.ControlMode.POSITION_CONTROL.value, motor_ip, 1)
+            aios.setPosition(0.5, 0, 0, True, motor_ip, 1)
+            #aios.setVelocity(1, 0, True, motor_ip, 1)
+            #aios.setCurrent(0.0500, True, motor_ip, 1)
+
+            time.sleep(3)
+
+            p, v, c = aios.getCVP(motor_ip, 1)
+            print('motor IP: ', motor_ip)
+            print('Position', p)
+            print('Velocity', v)
+            print('Current', c)
+
+            time.sleep(3)
+    else:
+        print("No motor IPs found.")
+else:
+    print("No connected addresses found.")
+
+time.sleep(3)
+
+if connected_addresses is not None:
+    connected_addresses.disable()
