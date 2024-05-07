12ab4f4185d4f077b9ba041db153b46bd1b85db2
yunzhewong <yunzhe.wong@gmail.com>
2024-05-07 20:11:46 +1000

    remove chatgpt from baseline server and define constants

--- ros-physical-interface/server.py
+++ ros-physical-interface/server.py
@@ -1,35 +1,26 @@
 import socket
+from serverConstants import HOST, PORT
 
-# Define the host and port on which the server will listen
-HOST = '127.0.0.1'
-PORT = 12345
-
-# Create a TCP/IP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the address and port
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((HOST, PORT))
 
-# Listen for incoming connections
-server_socket.listen(5)
+server_socket.listen(1)
 print(f"Server listening on {HOST}:{PORT}")
 
-while True:
-    # Accept a new connection
-    client_socket, client_address = server_socket.accept()
-    print(f"Connection from {client_address}")
+try:
+    while True:
+        client_socket, client_address = server_socket.accept()
 
-    # Receive data from the client
-    data = client_socket.recv(1024)
-    if not data:
-        break
+        data = client_socket.recv(1024)
+        if not data:
+            break
 
-    # Process received data (in this example, just echo it back)
-    print(f"Received: {data.decode()}")
-    client_socket.sendall(data)
+        print(f"Received: {data.decode()}")
 
-    # Close the connection with the client
-    client_socket.close()
+        returnBytes = "Received".encode()
+        client_socket.sendall(returnBytes)
 
-# Close the server socket
-server_socket.close()
+        client_socket.close()
+except KeyboardInterrupt:
+    server_socket.close()
