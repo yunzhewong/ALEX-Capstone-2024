ca9fde6560b47e6687a11e3862dcd0c9bde6e5c3
yunzhewong <yunzhe.wong@gmail.com>
2024-05-07 20:14:35 +1000

    Update client.py

--- ros-physical-interface/client.py
+++ ros-physical-interface/client.py
@@ -1,22 +1,14 @@
 import socket
+from serverConstants import HOST, PORT
 
-# Define the server's address and port
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 12345
-
-# Create a TCP/IP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 
-# Connect the socket to the server's address and port
-client_socket.connect((SERVER_HOST, SERVER_PORT))
-
-# Send data to the server
 message = "Hello, server!"
 client_socket.sendall(message.encode())
-
-# Receive data from the server
 data = client_socket.recv(1024)
-print(f"Received: {data.decode()}")
+if data.decode() != "Received":
+    raise Exception("Failed")
 
 # Close the connection with the server
 client_socket.close()
