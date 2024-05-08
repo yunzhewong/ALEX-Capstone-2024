d8784ae65bcde6b2bd3f67aa7387231a58201bc1
yunzhewong <yunzhe.wong@gmail.com>
2024-05-08 23:08:21 +1000

    tune server constants

--- physical_motor/serverConstants.py
+++ physical_motor/serverConstants.py
@@ -1,4 +1,4 @@
-# SHOULD MATCH WITH /src/alex_nodes/alex_nodes/serverConstants
+ROS_HOST = "0.0.0.0" # This does not have to match
 
-ROS_HOST = "127.0.0.1"
+# SHOULD MATCH WITH /src/alex_nodes/alex_nodes/serverConstants
 ROS_PORT = 12345
--- src/alex_nodes/alex_nodes/serverConstants.py
+++ src/alex_nodes/alex_nodes/serverConstants.py
@@ -1,4 +1,4 @@
-# SHOULD MATCH WITH /physical_motor/serverConstants
+ROS_HOST = "0.0.0.0" # This does not have to match
 
-ROS_HOST = "127.0.0.1"
+# SHOULD MATCH WITH /physical_motor/serverConstants
 ROS_PORT = 12345
