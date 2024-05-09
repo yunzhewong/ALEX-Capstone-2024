bc53a58e9cc8cfec7d911443ed6802bc1b21ae67
yunzhewong <yunzhe.wong@gmail.com>
2024-05-09 16:08:21 +1000

    halfway through changing motorcontroller to use multiple motors

---
+++ src/alex_nodes/alex_nodes/motor_pubsub_utils/MotorController.py
@@ -0,0 +1,51 @@
+from PIDController import PIDController
+from alex_nodes.classes import CommandObject
+from alex_nodes.commandTypes import CommandType
+from constants import MOTOR_TORQUE_CONSTANT, POSITION_GAIN, VEL_GAIN, VEL_INTEGRATOR_GAIN
+from std_msgs.msg import Float64MultiArray
+
+
+class MotorController():
+    def __init__(self, publisher):
+        self.publisher = publisher
+        self.positionPID = PIDController(POSITION_GAIN, 0, 0)
+        self.velocityPID = PIDController(VEL_GAIN, VEL_INTEGRATOR_GAIN, 0)
+        self.commandObject = None
+
+    def updateCommand(self, commandObject: CommandObject):
+        self.positionPID.clear()
+        self.velocityPID.clear()
+        self.commandObject = commandObject
+        if (commandObject.command == CommandType.Current):
+            return
+        
+        if (commandObject.command == CommandType.Position):
+            self.positionPID.setTarget(commandObject.value)
+        else:
+            self.velocityPID.setTarget(commandObject.value)
+
+    def updateState(self, time, position):
+        self.positionPID.updateLatest(time, position)
+        self.velocityPID.updateLatest(time, self.positionPID.e_dot)
+
+    def calculateTorque(self):
+        if not self.commandObject:
+            return 0
+        
+
+        if (self.commandObject.command == CommandType.Current):
+            return MOTOR_TORQUE_CONSTANT * self.commandObject.value
+        if (self.commandObject.command == CommandType.Position):
+            return self.positionPID.getControlValue()
+        if (self.commandObject.command == CommandType.Velocity):
+            return self.velocityPID.getControlValue()
+        raise Exception("No Command")
+    
+    def sendActuationCommand(self):
+        torque = -1 * self.calculateTorque()
+
+        msg = Float64MultiArray()
+        msg.data = [float(torque)]
+        msg.layout.data_offset = 0
+        self.publisher.publish(msg)
+
---
+++ src/alex_nodes/alex_nodes/motor_pubsub_utils/PIDController.py
@@ -0,0 +1,45 @@
+
+from alex_nodes.alex_nodes.motor_pubsub_utils.constants import FREQUENCY
+
+
+class PIDController():
+    def __init__(self, K_p, K_i, K_d):
+        self.K_p = K_p
+        self.K_i = K_i
+        self.K_d = K_d
+
+        self.target = None
+
+        self.previousTime = 0
+        self.previousValue = 0
+
+        self.e = 0
+        self.e_int = 0
+        self.e_dot = 0
+
+    def clear(self):
+        self.target = None
+        self.e = 0
+        self.e_int = 0
+        self.e_dot = 0
+
+         
+    def setTarget(self, target):
+        self.target = target
+        self.e = 0
+        self.e_int = 0
+        self.e_dot = 0
+
+    def updateLatest(self, time, value):
+        if self.target is None:
+            return
+        self.e = value - self.target 
+        self.e_dot = (value - self.previousValue) / (FREQUENCY * (time - self.previousTime))
+        self.e_int += self.e * (1 / FREQUENCY)
+
+        self.previousTime = time
+        self.previousValue = value
+
+    def getControlValue(self):
+        return self.K_p * self.e + self.K_d * self.e_dot + self.K_i * self.e_int
+        
