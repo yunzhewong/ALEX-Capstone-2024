6b8a04ea83115f51f90b7a2e44c70bdee2ed76db
yunzhewong <yunzhe.wong@gmail.com>
2024-05-09 18:44:59 +1000

    sample client to ros interaction

--- physical_motor/client.py
+++ physical_motor/client.py
@@ -4,7 +4,7 @@ from serverConstants import ROS_HOST, ROS_PORT
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((ROS_HOST, ROS_PORT))
 
-message = "Hello, server!"
+message = "10.10.10.17|0|5"
 client_socket.sendall(message.encode())
 data = client_socket.recv(1024)
 print(data)
--- physical_motor/serverConstants.py
+++ physical_motor/serverConstants.py
@@ -1,4 +1,4 @@
-ROS_HOST = "0.0.0.0" # This does not have to match
+ROS_HOST = "127.0.0.1"  # This does not have to match
 
 # SHOULD MATCH WITH /src/alex_nodes/alex_nodes/serverConstants
 ROS_PORT = 12345
