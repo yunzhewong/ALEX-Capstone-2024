fc89ebc5d05b04dc9528c2f3116659dd572b231d
yunzhewong <yunzhe.wong@gmail.com>
2024-05-20 22:28:22 +1000

    change test interface

--- physical_motor/testInterface.py
+++ physical_motor/testInterface.py
@@ -1,23 +1,22 @@
 import aiosv2
 import time
 import math
+import numpy as np
 
 if __name__ == "__main__":
     socket = aiosv2.AiosSocket()
     twinMotor = aiosv2.TwinMotor(socket)
     twinMotor.enable()
 
-    time.sleep(2)
-    motor = twinMotor.bottomMotor
-    motor.setPosition(0)
+    twinMotor.bottomMotor.setPosition(0)
     time.sleep(1)
-    motor.setPosition(math.pi / 8)
+    twinMotor.bottomMotor.setPosition(math.pi / 8)
     time.sleep(1)
-    motor.setPosition(0)
+    twinMotor.bottomMotor.setPosition(0)
     time.sleep(1)
-    motor.setPosition(-math.pi / 8)
+    twinMotor.bottomMotor.setPosition(-math.pi / 8)
     time.sleep(1)
-    motor.setPosition(0)
+    twinMotor.bottomMotor.setPosition(0)
     time.sleep(1)
 
     twinMotor.disable()
