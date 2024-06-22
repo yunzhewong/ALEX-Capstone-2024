ae326d673343903f526ef10d81c84c4b0289a2fe
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-06-22 19:05:19 +1000

    chirp identification test

--- matlab/chirp.m
+++ matlab/chirp.m
@@ -6,60 +6,8 @@ data = readmatrix(filename);
 times = data(:, 1);
 currents = data(:, 2);
 velocities = data(:, 3);
+positions = data(:, 4);
 
-frequencies = zeros(numel(times), 1);
-initial = 0;
-final = 25;
-t = 5;
-slope = (final - initial) / t;
-for i = 1:numel(times)
-    time = times(i);
-    frequency = slope * time + initial;
-    frequencies(i) = frequency;
-end
+chirp_identification(times, currents', velocities')
 
-simulated_velocity = sin(2*pi*frequencies.*times);
 
-% velocities = sin(10 * 2 * pi * times);
-% positions = data(:, 4);
-% 
-% figure
-% plot(times, currents, "r-")
-% 
-% figure
-% plot(times, velocities, "b-")
-% plot(times, simulated_velocity, "g-")
-% 
-% figure
-% plot(times, positions, "r-")
-
-T = 0.003;
-Fs = 1 / T;
-[pks, locs] = findpeaks(velocities);
-
-magnitudes = zeros(numel(locs) - 1, 1);
-frequencies = zeros(numel(locs) - 1, 1);
-
-for i = 1:numel(locs) - 1
-    start = locs(i);
-    finish = locs(i + 1);
-    L = finish - start;
-    section = velocities(start:finish);
-    ffted  = fft(section);
-    
-    P2 = abs(ffted/L);
-    P1 = P2(1:L/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-    
-    f = Fs/L*(0:(L/2));
-    [M, I] = max(P1);
-    frequencies(i) = f(I);
-    magnitudes(i) = M;
-end
-
-figure
-scatter(frequencies, magnitudes);
-
-db = -20* log10(magnitudes);
-figure
-scatter(frequencies, db)
---
+++ matlab/chirp_identification.m
@@ -0,0 +1,44 @@
+function chirp_identification(t, u, y)
+    % as the fourier transform creates a frequency domain plot from 0Hz to the
+    % nyquist frequency of the system (e.g. sample_freq / 2), we need to use
+    % these values to reconstruct the graph
+    sample_time = t(2) - t(1);
+    sample_freq = 1 / sample_time;
+    total_points = numel(t);
+
+    % vector checks
+    assert(size(u, 1) == 1 && size(u, 2) == total_points)
+    assert(size(y, 1) == 1 && size(y, 2) == total_points)
+
+    % fft response, taking the results and frequency-wise taking the ratio
+    uffted = fft(u);
+    yffted = fft(y);
+    ratio = yffted ./ uffted;
+    
+    % reconstruction of the entire FFT domain. From  0 to sample_freq with the
+    % same number of points as the input signal
+    full_f = sample_freq / total_points * (0:total_points- 1);
+    
+    % conversion to a single sided amplitude spectrum. There is no need to
+    % rescale as a ratio is being taken
+    P2 = abs(ratio);
+    P1 = P2(1:total_points/2+1);
+    half_f = full_f(1:total_points / 2 + 1);
+    
+    theta = angle(ratio) / pi * 180;
+
+    % bode is typically plotted as w (rads^-1) vs M (db)
+    omega = 2*pi*half_f;
+
+    figure
+    semilogx(omega, 20*log10(P1))
+    title("Magnitude Plot")
+    xlabel("Frequency (rads^-1)")
+    ylabel("Magnitude (db)")
+
+    figure
+    semilogx(omega, theta(1:total_points / 2 + 1));
+    title("Phase Plot vs Frequency")
+    xlabel("Frequency (rads^-1)")
+    ylabel("Phase (degrees)")
+end
