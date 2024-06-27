f971707a47601f281ee5da1b532c818ca4311a43
yunzhewong <yunzhe.wong@gmail.com>
2024-06-27 18:01:43 +1000

    working encoder reader

--- src/alex_nodes_cpp/src/encoder_reader.cpp
+++ src/alex_nodes_cpp/src/encoder_reader.cpp
@@ -4,12 +4,14 @@
 #include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 
 using std::placeholders::_1;
+using std_msgs::msg::Float64MultiArray;
 using sensor_msgs::msg::JointState;
 
-bool doublesEqual(double d1, double d2) {
+bool doubleEqual(double d1, double d2){
     return std::abs(d1 - d2) < 1e-9;
 }
 
@@ -17,20 +19,28 @@ class MinimalSubscriber : public rclcpp::Node
 {
   private:
     rclcpp::Subscription<JointState>::SharedPtr joint_sub;
+    rclcpp::Subscription<Float64MultiArray>::SharedPtr torque_sub;
     std::ofstream file;
     double starting_time;
+    double torque;
+    bool torque_read;
 
   public:
     MinimalSubscriber()
     : Node("minimal_subscriber")
     {
       joint_sub = this->create_subscription<JointState>(
-      "/joint_states", 10, std::bind(&MinimalSubscriber::on_joint_msg, this, _1));
+        "/joint_states", 10, std::bind(&MinimalSubscriber::on_joint_msg, this, _1));
+
+      torque_sub = this->create_subscription<Float64MultiArray>(
+        "/bottom_motor_controller/commands", 10, std::bind(&MinimalSubscriber::on_torque, this, _1));
 
       file.open("data.csv");
       file << "Times (s), Currents (A), Velocities (rads^-1), Positions(rads)" << '\n';
       
-      starting_time = -1.0;
+      starting_time = -1;
+      torque = 0;
+      torque_read = false;
     }
     ~MinimalSubscriber() {
         file.close();
@@ -38,13 +48,21 @@ class MinimalSubscriber : public rclcpp::Node
 
   private:
     void on_joint_msg(const JointState & msg) {
+        if (!torque_read) {
+            return;
+        }
         auto timestamp = msg.header.stamp;
         double time = timestamp.sec + (timestamp.nanosec / 1e9);
-        if (doublesEqual(starting_time, -1.0)) {
+        if (doubleEqual(starting_time, -1.0)) {
             starting_time = time;
         }
         double relative_time = time - starting_time;
         file << relative_time << ',' << 0  << ',' << msg.velocity[0] << ',' << msg.position[0] << '\n';
+    }
+
+    void on_torque(const Float64MultiArray &msg) {
+        torque_read = true;
+        torque = msg.data[0];
     }
 };
 
