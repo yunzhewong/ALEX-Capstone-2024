bea09d2f0c3dfb6f7f123b60ca6be79614dcb002
yunzhewong <yunzhe.wong@gmail.com>
2024-06-27 16:37:42 +1000

    reorder syntax

--- src/alex_nodes_cpp/src/simple_publisher.cpp
+++ src/alex_nodes_cpp/src/simple_publisher.cpp
@@ -9,24 +9,27 @@
 using namespace std::chrono_literals;
 
 class MinimalPublisher : public rclcpp::Node {
- private:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  private:
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
 
-  void timer_callback() {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
-  }
+  public:
+    MinimalPublisher() : Node("minimal_publisher"), count_(0) {
+      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      timer_ = this->create_wall_timer(
+          500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
 
- public:
-  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-        500ms, std::bind(&MinimalPublisher::timer_callback, this));
-  }
+  private:
+    void timer_callback() {
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(message);
+    }
+
+ 
 };
 
 int main(int argc, char* argv[]) {
