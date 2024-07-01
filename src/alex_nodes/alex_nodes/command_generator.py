c1fd7f0c5c37d691f274fe701a7c6befbb96c270
yunzhewong <yunzhe.wong@gmail.com>
2024-07-01 22:16:01 +1000

    joint position introduced in as well

--- src/alex_nodes/alex_nodes/command_generator.py
+++ src/alex_nodes/alex_nodes/command_generator.py
@@ -4,6 +4,8 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from alex_interfaces.msg import Command
+from sensor_msgs.msg import JointState
+
 import sys
 package_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(package_dir)
@@ -16,12 +18,17 @@ class CommandGenerator(Node):
     def __init__(self):
         super().__init__("command_generator")
 
-        self.publisher = self.create_publisher(Command, f"/commands", 10)
-        self.timer = self.create_timer(TIMER_PERIOD, self.send_command)
+        self.publisher = self.create_publisher(Command, "/commands", 10)
+        self.publish_timer = self.create_timer(TIMER_PERIOD, self.send_command)
+
+        self.subscriber = self.create_subscription(JointState, "/joint_states", self.read_time, 10)
         self.time = 0
 
         self.types = [CommandType.Current.value, CommandType.Current.value]
         self.values = [0, 0]
+
+        self.positions = [0, 0]
+        self.velocities = [0, 0]
 
     def send_command(self):
         self.commands(self.time)
@@ -32,6 +39,12 @@ class CommandGenerator(Node):
         msg.values = self.values
         self.publisher.publish(msg)
         self.time += TIMER_PERIOD
+
+    def read_time(self, msg: JointState):
+        timestamp = msg.header.stamp
+        self.time = timestamp.sec + timestamp.nanosec / 1e9
+        self.positions = msg.position
+        self.velocities = msg.velocity
 
     def commands(self, t):
         val = 5 * math.sin(2 * math.pi * 0.1 * t)
