41cbf2b4f1a6252b7ab01b746f4dbc68714eef37
yunzhewong <yunzhe.wong@gmail.com>
2024-07-09 13:30:37 +1000

    [yz]: simplify socket

--- physical_motor/aiosv2/AiosSocket.py
+++ physical_motor/aiosv2/AiosSocket.py
@@ -4,24 +4,28 @@ import json
 import socket
 from aiosv2.constants import PORT_srv
 
-class PhysicalSocket:
+class AiosSocket:
+    communicationSocket: socket.socket
     NETWORK = "10.10.10.255"
 
+
     def __init__(self):
-        physicalSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        physicalSocket.settimeout(2)
-        physicalSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.socket = physicalSocket
+        print(f"Socket Active")
+
+        newSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        newSocket.settimeout(2)
+        newSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.communicationSocket = newSocket
 
     def assertConnectedAddresses(self, expectedIPs):
-        self.socket.sendto(
+        self.communicationSocket.sendto(
             "Is any AIOS server here?".encode("utf-8"), (self.NETWORK, PORT_srv)
         )
 
         foundIPs = []
         while True:
             try:
-                _, address = self.socket.recvfrom(1024)
+                _, address = self.communicationSocket.recvfrom(1024)
                 foundIPs.append(address[0])
             except socket.timeout:  # fail after 1 second of no activity
                 for ip in expectedIPs:
@@ -29,49 +33,26 @@ class PhysicalSocket:
                         raise Exception(f"Missing IP: {ip}")
                 return
 
-    def sendJSON(self, ip, port, data):
+    def sendJSON(self, ip: str, port: int, data: dict):
         json_str = json.dumps(data)
-        self.socket.sendto(json_str.encode(), (ip, port))
+        self.communicationSocket.sendto(json_str.encode(), (ip, port))
 
     def readJSON(self):
-        data, addr = self.socket.recvfrom(1024)
+        data, addr = self.communicationSocket.recvfrom(1024)
 
         json_obj = json.loads(data.decode("utf-8"))
         ip = addr[0]
         return (json_obj, ip)
 
     def sendBytes(self, ip, port, bytesToSend):
-        self.socket.sendto(bytesToSend, (ip, port))
-
-    def readBytes(self):
-        data, address = self.socket.recvfrom(1024)
-        return data
-
-class AiosSocket:
-    physicalSocket: PhysicalSocket
-
-    def __init__(self):
-        print(f"Physical Socket Active")
-
-        self.physicalSocket = PhysicalSocket()
-
-    def assertConnectedAddresses(self, expectedIPs):
-        self.physicalSocket.assertConnectedAddresses(expectedIPs)
-
-    def sendJSON(self, ip: str, port: int, data: dict):
-        self.physicalSocket.sendJSON(ip, port, data)
-
-    def readJSON(self):
-        return self.physicalSocket.readJSON()
-
-    def sendBytes(self, ip: str, port: int, bytesToSend: bytes):
-        self.physicalSocket.sendBytes(ip, port, bytesToSend)
+        self.communicationSocket.sendto(bytesToSend, (ip, port))
 
     def readBytes(self) -> bytes:
-        return self.physicalSocket.readBytes()
+        data, address = self.communicationSocket.recvfrom(1024)
+        return data
 
     def changeState(self, ip, stateValue):
-        self.physicalSocket.sendJSON(
+        self.sendJSON(
             ip,
             PORT_srv,
             {
@@ -82,7 +63,7 @@ class AiosSocket:
             },
         )
         try:
-            self.physicalSocket.readJSON()
+            self.readJSON()
         except:
             # its fine to not read anything, as we just want to collect it
             pass
