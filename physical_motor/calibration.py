30cd7e1d1c4674d31cfa620d05d666c914c8ecdb
yunzhewong <yunzhe.wong@gmail.com>
2024-08-15 15:11:58 +1000

    add position limits

--- physical_motor/calibration.py
+++ physical_motor/calibration.py
@@ -1,6 +1,8 @@
 from aiosv2.TwinMotor import calibrate_twin_motor
 from aiosv2.RightKneeExoMotor import calibrate_right_knee_exomotor
+from aiosv2.Exoskeleton import calibrate_exoskeleton
 
 if __name__ == "__main__":
-    # calibrate_twin_motor()
-    calibrate_right_knee_exomotor()
+    calibrate_twin_motor()
+    # calibrate_right_knee_exomotor()
+    # calibrate_exoskeleton()
