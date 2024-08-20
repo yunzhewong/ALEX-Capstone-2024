a82f459d9e6bfc195fa4838d8032e4862adf2ba1
yunzhewong <yunzhe.wong@gmail.com>
2024-08-20 16:07:47 +1000

    minor change

--- physical_motor/zero_position.py
+++ physical_motor/zero_position.py
@@ -9,7 +9,11 @@ if __name__ == "__main__":
         trajectory.slow_move_to_pos(exoskeleton.leftAbductor, 0)
         trajectory.slow_move_to_pos(exoskeleton.rightExtensor, 0)
         trajectory.slow_move_to_pos(exoskeleton.leftExtensor, 0)
-        trajectory.slow_move_to_pos(exoskeleton.rightKnee, 0.05)
         trajectory.slow_move_to_pos(exoskeleton.leftKnee, -0.05)
+        if runningTime < 5:
+            trajectory.slow_move_to_pos(exoskeleton.rightKnee, 0.05)
+        else:
+            trajectory.slow_move_to_pos(exoskeleton.rightKnee, 1)
 
-    setup_teardown_motor_combination(Exoskeleton(), func, 100)
+
+    setup_teardown_motor_combination(Exoskeleton(), func, 10)
