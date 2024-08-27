e80327cd807447617c1b4983ed0de9299fe0d6a6
yunzhewong <yunzhe.wong@gmail.com>
2024-08-27 10:49:57 +1000

    right extensor testing

--- physical_motor/torque_testing.py
+++ physical_motor/torque_testing.py
@@ -12,12 +12,12 @@ def stop_collection(cvp: CVP):
     return cvp.position > (math.pi / 2)
 
 
-MAX_TIME = 20
+MAX_TIME = 15
 PAUSE_TIME = 1
 
-START_MAGNITUDE = 1.65
+START_MAGNITUDE = 3.95
 INCREMENT_MAGNITUDE = 0.05
-END_MAGNITUDE = 1.7
+END_MAGNITUDE = 5.00
 
 COUNT = int((END_MAGNITUDE - START_MAGNITUDE) / INCREMENT_MAGNITUDE)
 currents = [START_MAGNITUDE + INCREMENT_MAGNITUDE * i for i in range(COUNT)]
@@ -72,7 +72,7 @@ class BulkDataBatcher:
                 self.collect_index += 1
                 self.initialised = False
 
-            connection.setCurrent(current)
+            connection.setCurrent(-1 * current)
         elif self.state == State.Paused:
             if not self.initialised:
                 self.pause_end_time = t + PAUSE_TIME
@@ -89,7 +89,7 @@ class BulkDataBatcher:
 
             trajectory.slow_move_to_pos(connection, 0)
             
-            if cvp.position < 0.05:
+            if abs(cvp.position) < 0.05:
                 self.post_pause_state = State.Collecting
                 self.state = State.Paused
                 self.initialised = False
@@ -102,9 +102,9 @@ if __name__ == "__main__":
     def func(exoskeleton: Exoskeleton, runningTime: float):
         trajectory.slow_move_to_pos(exoskeleton.rightAbductor, 0)
         trajectory.slow_move_to_pos(exoskeleton.leftAbductor, 0)
-        trajectory.slow_move_to_pos(exoskeleton.rightExtensor, 0)
+        dataBatcher.at_time(runningTime, exoskeleton.rightExtensor)
         trajectory.slow_move_to_pos(exoskeleton.leftExtensor, 0)
-        dataBatcher.at_time(runningTime, exoskeleton.rightKnee)
+        trajectory.slow_move_to_pos(exoskeleton.rightKnee, 0)
         trajectory.slow_move_to_pos(exoskeleton.leftKnee, 0)
 
 
