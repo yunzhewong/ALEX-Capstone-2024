4870ae2d825ee2bce0f58e7ba92212d570535f6e
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-09-27 13:08:15 +1000

    [yz]: visualisation and writing of reference trajectory

--- matlab/visualisation/vis.m
+++ matlab/visualisation/vis.m
@@ -1,38 +1,53 @@
 close all
-[times, currents, velocities, positions] = read_data("./abductor.csv");
-[times_2, currents_2, velocities_2, positions_2] = read_data("./abduct.csv");
+[times, currents, velocities, positions] = read_data("./knee.csv");
+[times_2, currents_2, velocities_2, positions_2] = read_data("./kneereal.csv");
 
-% extensor
-t2 = 23.71;
-t1 = 14.43;
+reference = readmatrix('kneetraj.csv');
+ref_times = reference(:, 1);
+ref_velocities = reference(:, 2);
+ref_pos = reference(:, 3);
+
+% % extensor
+% t2 = 23.71;
+% t1 = 14.43;
 
 % % abductor
 % t2 = 23.942
 % t1 = 14.60
 
-% % knee
-% t2 = 29.061;
-% t1 = 19.63;
+% knee
+t2 = 10.363;
+t1 = 0.997;
 
 diff = t2 - t1;
 adjust_times = times - diff;
 
 fig = figure;
-fig.Position = [50, 50, 1200, 700];
-subplot(3, 1, 1)
-hold on
-plot(adjust_times, currents)
-plot(times_2, currents_2)
-xlim([0 86])
+fig.Position = [50, 50, 900, 750];
 
-subplot(3, 1, 2)
-hold on
-plot(adjust_times, velocities)
-plot(times_2, velocities_2)
-xlim([0 86])
+tiledlayout(3, 1, "TileSpacing","compact", "Padding","compact");
 
-subplot(3, 1, 3)
+nexttile
 hold on
-plot(adjust_times, positions)
-plot(times_2, positions_2)
-xlim([0 86])
+plot(adjust_times, currents, "Color", "#4DBEEE")
+plot(times_2, currents_2, "Color", "#7E2F8E")
+xlim([0 40])
+ylabel("Current (A)")
+
+nexttile
+hold on
+plot(ref_times, ref_velocities, "LineWidth", 2, "LineStyle", '- -', "Color", "r")
+plot(adjust_times, velocities, "Color", "#4DBEEE")
+plot(times_2, velocities_2, "Color", "#7E2F8E")
+xlim([0 35])
+ylabel("Velocity (rads^{-1})")
+
+nexttile
+hold on
+plot(ref_times, ref_pos, "LineWidth", 2, "LineStyle", '- -', "Color", "r")
+plot(adjust_times, positions, "Color", "#4DBEEE")
+plot(times_2, positions_2, "Color", "#7E2F8E")
+
+xlim([0 40])
+ylabel("Position (rad)")
+xlabel("Time (s)")
--- physical_motor/trajectorycsv.py
+++ physical_motor/trajectorycsv.py
@@ -1,62 +1,18 @@
-from aiosv2.Exoskeleton import Exoskeleton
-from aiosv2.Controllers import CascadeController
-from aiosv2.ControlLoop import setup_teardown_motor_combination
-from aiosv2.RightKneeExoMotor import RightKneeExoMotor
-from classes.DataLog import CVPPlot
 from rebuild import EXO24Trajectory
-import trajectory as t
 
 DURATION = 86
 
 
-class State:
-    def __init__(self):
-        self.log = CVPPlot()
-        self.last_time = 0
-        self.initialised = False
-
-
 if __name__ == "__main__":
     trajectory = EXO24Trajectory.Exo24Trajectory()
+    file = open("kneetraj.csv", "w")
+    file.write("Time, Velocity, Position\n")
 
-    leftAbductorController = CascadeController()
-    rightAbductorController = CascadeController()
-    leftExtensorController = CascadeController()
-    rightExtensorController = CascadeController()
-    leftKneeController = CascadeController()
-    rightKneeController = CascadeController()
+    time = 0
 
-    state = State()
+    while time <= DURATION:
+        reference_position, reference_velocity = trajectory.get_state(time)
+        file.write(f"{time}, {reference_velocity[5]}, {reference_position[5]}\n")
+        time += 0.001
 
-    def func(exoskeleton: Exoskeleton, runningTime: float):
-        reference_position, reference_velocity = trajectory.get_state(runningTime)
-
-        dt = runningTime - state.last_time
-
-        leftAbductorController.set_reference(
-            exoskeleton.leftAbductor, reference_position[0], reference_velocity[0], dt
-        )
-        rightAbductorController.set_reference(
-            exoskeleton.rightAbductor, reference_position[1], reference_velocity[1], dt
-        )
-        leftExtensorController.set_reference(
-            exoskeleton.leftExtensor, reference_position[2], reference_velocity[2], dt
-        )
-        rightExtensorController.set_reference(
-            exoskeleton.rightExtensor, reference_position[3], reference_velocity[3], dt
-        )
-        leftKneeController.set_reference(
-            exoskeleton.leftKnee, reference_position[4], reference_velocity[4], dt
-        )
-        rightKneeController.set_reference(
-            exoskeleton.rightKnee, reference_position[5], reference_velocity[5], dt
-        )
-
-        state.last_time = runningTime
-
-        cvp = exoskeleton.rightKnee.getCVP()
-        state.log.addCVP(runningTime, cvp)
-
-    setup_teardown_motor_combination(Exoskeleton(), func, DURATION)
-
-    state.log.plot()
+    file.close()
